10101086edaea296396501be830b7ed11957c9bb
LeaFraioli <lea.fraioli@ynov.com>
2020-03-19 13:52:34 +0100

    initial commit

---
+++ Assets/Scenes/RotateObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+	
+    // Déclaration des variables de vitesse
+	public float xSpeed = 0.0f;
+	public float ySpeed = 0.0f;
+	public float zSpeed = 0.0f;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Rotate(
+            xSpeed * Time.deltaTime,
+            ySpeed * Time.deltaTime,
+            zSpeed * Time.deltaTime
+       );
+	}
+}
